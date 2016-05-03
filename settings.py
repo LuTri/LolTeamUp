1dc33f354dda2c5053c955c17a332b28226e2372
LuTri <tristan.lucas.de@gmail.com>
2016-05-03 10:20:57 +0200

    Adding Standardteamconfigs

--- settings.py
+++ settings.py
@@ -1,6 +1,28 @@
 API_KEY=NotImplemented
 DEFAULT_REGION = 'euw'
 
+TEAM_CONFIGS = [
+	{
+		'configname': 'STANDARD',
+		'tags': [
+			'Tank',
+			'Fighter'
+			'Mage',
+			'Support',
+			'Marksman',
+		]
+	}, {
+		'configname': 'ADVANCED',
+		'tags': [
+			'Fighter',
+			'Tank',
+			'Assassin',
+			'Support',
+			'Marksman'
+		]
+	}
+]
+
 try:
 	from local_settings import *
 except:
